4b3dffab0a7ef836b8963e4b07b5ba9f54ad3648
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-06-03 15:53:16 +0200

    [FIX] correzioni nella chiamata ajax e nel nome delle props

--- components/FilmCard.jsx
+++ components/FilmCard.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 const FilmCard = ({ film }) => {
     // destrutturazione 
-    const { id, title, author, excerpt, cover } = film;
+    const { id, title, director, excerpt, image } = film;
     return (
         <div className="col-12 col-md-6 col-lg-4">
             <div className="card">
                 <div className="card-img-top">
-                    <img src={cover} alt="film" className='img-fluid' />
+                    <img src={image} alt="film" className='img-fluid' />
                 </div>
                 <div className="card-body">
                     <h3 className='text-primary'>{title}</h3>
-                    <h5><em>{author}</em></h5>
+                    <h5><em>{director}</em></h5>
                     <p>{excerpt}</p>
                     <Link className="btn btn-primary" to={`/films/${id}`}>Esplora</Link>
                 </div>
--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,154 +1,21 @@
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import FilmCard from "../components/FilmCard";
 import axios from "axios";
-const initialFilms = [
-    {
-        id: 1,
-        title: "Titolo 1",
-        author: "Autore 1",
-        excerpt: "Lorem ipsum dolor sit amet",
-        cover: "https://picsum.photos/500/300",
-        review: [
-            {
-                id: 1,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 2,
-                author: "Autore 1"
-            },
-            {
-                id: 2,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 4,
-                author: "Autore 2"
-            },
-            {
-                id: 3,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 3,
-                author: "Autore 3"
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: "Titolo 2",
-        author: "Autore 2",
-        excerpt: "Lorem ipsum dolor sit amet",
-        cover: "https://picsum.photos/500/300",
-        review: [
-            {
-                id: 1,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 2,
-                author: "Autore 1"
-            },
-            {
-                id: 2,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 4,
-                author: "Autore 2"
-            },
-            {
-                id: 3,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 3,
-                author: "Autore 3"
-            }
-        ]
-    },
-    {
-        id: 3,
-        title: "Titolo 3",
-        author: "Autore 3",
-        excerpt: "Lorem ipsum dolor sit amet",
-        cover: "https://picsum.photos/500/300",
-        review: [
-            {
-                id: 1,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 2,
-                author: "Autore 1"
-            },
-            {
-                id: 2,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 4,
-                author: "Autore 2"
-            },
-            {
-                id: 3,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 3,
-                author: "Autore 3"
-            }
-        ]
-    },
-    {
-        id: 4,
-        title: "Titolo 4",
-        author: "Autore 4",
-        excerpt: "Lorem ipsum dolor sit amet",
-        cover: "https://picsum.photos/500/300",
-        review: [
-            {
-                id: 1,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 2,
-                author: "Autore 1"
-            },
-            {
-                id: 2,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 4,
-                author: "Autore 2"
-            },
-            {
-                id: 3,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 3,
-                author: "Autore 3"
-            }
-        ]
-    },
-    {
-        id: 5,
-        title: "Titolo 5",
-        author: "Autore 5",
-        excerpt: "Lorem ipsum dolor sit amet",
-        cover: "https://picsum.photos/500/300",
-        review: [
-            {
-                id: 1,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 2,
-                author: "Autore 1"
-            },
-            {
-                id: 2,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 4,
-                author: "Autore 2"
-            },
-            {
-                id: 3,
-                text: "Lorem ipsum dolor sit amet",
-                vote: 3,
-                author: "Autore 3"
-            }
-        ]
-    }
-];
 
-const HomePage = (film) => {
+const HomePage = () => {
     // definisco la variabile di stato
-    const [films, setFilms] = useState(initialFilms)
+    const [films, setFilms] = useState([])
+
     // definizione funzione che recupera i dati dal db tramite chiamata ajax con axios
-    useEffect(() => {
+    const fetchFilm = () => {
         axios.get('http://127.0.0.1:3000/api/films/').then((resp) => {
-            console.log(resp.data)
+            setFilms(resp.data)
         }).catch((err) => { console.log(err) })
-    }, [])
+    }
+
+    // utilizzo l'hook useEffect per mostrare i film al caricamento della pagina
+    useEffect(fetchFilm, [])
+
     return (
         <>
             <h1 className='text-secondary'>BOOLFILMS</h1>
